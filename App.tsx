ebf7113bc8d3fc657b95dd0bc5c08350bf2cb90e
do-ald533 <douglas.aldred533@gmail.com>
2024-03-02 18:51:04 -0300

    testing server

--- App.tsx
+++ App.tsx
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const isHermes = () => !!global.HermesInternal;
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>teste</Text>
       <StatusBar style="auto" />
     </View>
   );
